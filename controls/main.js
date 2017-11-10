50fe321ad1bc75c4284c80290c7776fca0461e37
mayweipwc <33535593+mayweipwc@users.noreply.github.com>
2017-11-10 11:30:45 +0800

    add files to master

---
+++ controls/main.js
@@ -0,0 +1,17 @@
+﻿var app = angular.module("myApp", ["ngRoute", "ngCookies"]);
+app.config(function ($routeProvider) {
+    $routeProvider
+        .when("/login", {
+            templateUrl: "login.html",
+            controller: "appController"
+        })
+        .when("/index", {
+            templateUrl: "index.html",
+            controller: "appController"
+        })
+        .otherwise({
+            redirectTo: "/login"
+         }
+        )
+        ;
+});
