60a3397a0b45f3aa95a8e14d671ce37df1678b32
mayweipwc <33535593+mayweipwc@users.noreply.github.com>
2017-11-30 13:18:42 +0800

    Add files via upload

---
+++ controls/app.js
@@ -0,0 +1,18 @@
+﻿var app = angular.module("myApp", ["ngRoute", "ngCookies"]);
+app.config(['$routeProvider', '$locationProvider', function AppConfig($routeProvider, $locationProvider) {
+    $routeProvider
+        .when("/login", {
+            templateUrl: "./views/Login.html",
+            controller: "appController"
+        })
+        .when("/", {
+            templateUrl: "./views/Main.html",
+            controller: "appController"
+        })
+        .otherwise({
+            redirectTo: "/"
+        }
+        );
+    $locationProvider.hashPrefix('');
+
+}]);
